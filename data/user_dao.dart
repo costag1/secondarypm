a47b7becb4c6f1302e07cf9ca078bf5332635266
costag1 <103394333+costag1@users.noreply.github.com>
2022-10-11 08:05:08 -0300

    Add files via upload

---
+++ data/user_dao.dart
@@ -0,0 +1,42 @@
+import 'package:prev/domain/user.dart';
+//import 'package:sqflite/sqflite.dart';
+
+class UserDao {
+
+  Future<void> salvarUser({required User user}) async {
+    DBHelper dbHelper = DBHelper();
+    Database db = await dbHelper.initDB();
+
+    await db.insert('user', user.toJson());
+  }
+
+  Future<bool> autenticar({required String user, required String password}) async {
+    DBHelper dbHelper = DBHelper();
+    Database db = await dbHelper.initDB();
+
+    String sql = 'SELECT * '
+        'FROM user '
+        'WHERE username = ? AND password = ?;';
+
+    final result = await db.rawQuery(sql, [user, password]);
+
+    return result.isNotEmpty;
+  }
+
+  listarUsers() async {
+    DBHelper dbHelper = DBHelper();
+    Database db = await dbHelper.initDB();
+
+    String sql = 'SELECT * FROM user;';
+    final result = await db.rawQuery(sql);
+
+    List<User> lista = <User>[];
+    for(var json in result){
+      User usuario = User.fromJson(json);
+      lista.add(usuario);
+    }
+
+    return lista;
+  }
+
+}
